a5da14eb85daf03c8846b78382993df6d25890c0
Krish Mittal <krishmittal212005@gmail.com>
2024-08-05 14:30:36 +0530

    bring the swiftui componnt to view controller

--- VAIDid13/ViewController.swift
+++ VAIDid13/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let imageSliderView = ImageSliderView()
+        
+        let hostingController = UIHostingController(rootView: imageSliderView)
+        
+        addChild(hostingController)
+        
+        view.addSubview(hostingController.view)
+        
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        hostingController.didMove(toParent: self)
     }
-
-
 }
 
